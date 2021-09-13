1d3580acb134825491de8f5eed81cd3af947ec49
xhmyjae <tomatis.mt@gmail.com>
2021-09-13 18:43:30 +0200

    first commit

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for alphabet := 'a'; alphabet <= 'z'; alphabet++ {
+	for alphabet := 'z'; alphabet >= 'a'; alphabet-- {
 		z01.PrintRune(alphabet)
 	}
 	z01.PrintRune('\n')
