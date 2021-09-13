5131e2b30dd9c7d175e44e6c7236fad9adf2ef4b
xhmyjae <tomatis.mt@gmail.com>
2021-09-13 22:14:35 +0200

    first commit

--- isnegative.go
+++ isnegative.go
@@ -1,12 +1,5 @@
 package main
 
-/*
-renvoie T si l'entier passer en paramètre est inférieur à 0, et F s'il est supérieur ou égal à 0.
-:param i: (int)
-:return: (bool) 
-	T -> l'int est négatif
-	F -> l'int est positif ou égal à 0 
-*/
 func IsNegative(i int) {
 	if i < 0 {
 		println("T")
@@ -16,7 +9,6 @@ func IsNegative(i int) {
 }
 
 func main() {
-	/* permet de tester la fonction IsNegative */
 	IsNegative(3)
 	IsNegative(-2)
 	IsNegative(0)
