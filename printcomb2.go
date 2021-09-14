267dfce13aa77e95ba743f419aa5358c4c933d83
xhmyjae <tomatis.mt@gmail.com>
2021-09-14 14:15:55 +0200

    first commit

--- printcomb2.go
+++ printcomb2.go
@@ -4,12 +4,12 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for digit := 0; digit < 89; digit++ {
-		a := digit / 100
-		b := (digit / 10) % 10
+		a := digit
+		b := digit
 		if a < b {
 			z01.PrintRune(rune(a + 48))
 			z01.PrintRune(rune(b + 48))
-			if a == 8 && b == 9 {
+			if a == 98 && b == 99 {
 				z01.PrintRune('\n')
 			} else {
 				z01.PrintRune(44)
