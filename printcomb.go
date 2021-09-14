d251f6868921a7b1981e11429b7e282d81174894
xhmyjae <tomatis.mt@gmail.com>
2021-09-14 12:27:03 +0200

    first commit

--- printcomb.go
+++ printcomb.go
@@ -1,13 +1,15 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func PrintComb() {
 	for digit := 0; digit < 899; digit++ {
 		a := digit / 100
 		b := (digit / 10) % 10
 		c := digit % 10
 		if a < b && b < c {
-			print(a, b, c)
-			print(", ")
+			z01.PrintRune(a, b, c)
+			z01.PrintRune(44, 27)
 		}
 	}
 }
