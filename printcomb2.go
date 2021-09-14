1f06744648e70f5f436f52aaac9114874824f6ef
xhmyjae <tomatis.mt@gmail.com>
2021-09-14 15:53:12 +0200

    first commit

--- printcomb2.go
+++ printcomb2.go
@@ -2,7 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintDigitComb() {
+func PrintComb2() {
 	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
