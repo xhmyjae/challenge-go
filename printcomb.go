932de9f87741caf3a8aa4c99a0379be28c797bc7
xhmyjae <tomatis.mt@gmail.com>
2021-09-14 13:59:00 +0200

    first commit

--- printcomb.go
+++ printcomb.go
@@ -8,9 +8,12 @@ func PrintComb() {
 		b := (digit / 10) % 10
 		c := digit % 10
 		if a < b && b < c {
-			z01.PrintRune(a + 48)
-			z01.PrintRune(b + 48)
-			z01.PrintRune(c + 48)
+			a += 48
+			b += 48
+			c += 48
+			z01.PrintRune(a)
+			z01.PrintRune(b)
+			z01.PrintRune(c)
 			z01.PrintRune(44)
 			z01.PrintRune(32)
 		}
