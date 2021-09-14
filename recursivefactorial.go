4d6cfcec94f89b275a40d1c46f2ca32616a50d1a
xhmyjae <tomatis.mt@gmail.com>
2021-09-14 21:57:33 +0200

    first commit

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,6 +3,8 @@ package piscine
 func RecursiveFactorial(nb int) int {
 	if nb < 0 || nb > 100000 {
 		return 0
+	} else if nb == 0 {
+		return 1
 	} else {
 		res := 1
 		res = RecursiveFactorial(nb+1) * res
