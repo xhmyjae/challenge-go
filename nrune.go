a2e101e555d9717976d936bdcf2a518fc3ada57f
xhmyjae <tomatis.mt@gmail.com>
2021-09-15 16:44:40 +0200

    nrune

--- nrune.go
+++ nrune.go
@@ -3,9 +3,13 @@ package piscine
 func NRune(s string, n int) rune {
 	index := 0
 	var letter rune
-	for index, letter = range s {
-		if index == n-1 {
-			break
+	if n > len(s) {
+		return 0
+	} else {
+		for index, letter = range s {
+			if index == n-1 {
+				break
+			}
 		}
 	}
 	return letter
