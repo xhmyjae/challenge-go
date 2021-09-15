25f479448e858491fd1ffc1d321d24573f282451
xhmyjae <tomatis.mt@gmail.com>
2021-09-15 10:24:08 +0200

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -3,7 +3,11 @@ package piscine
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
+	} else if index == 0 {
+		return 0
+	} else if index == 1 {
+		return 1
 	} else {
-		return index + Fibonacci(index-1)
+		return Fibonacci(index-2) + Fibonacci(index-1)
 	}
 }
