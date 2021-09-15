4101bd5b63949961ec9e4a747c0e139b9db37781
xhmyjae <tomatis.mt@gmail.com>
2021-09-15 16:48:25 +0200

    nrune

--- nrune.go
+++ nrune.go
@@ -4,12 +4,9 @@ func NRune(s string, n int) rune {
 	index := 0
 	var letter rune
 	for index, letter = range s {
-		if n > index {
-			return 0
-		} else {
-			if index == n-1 {
-				break
-			}
+		if index == n-1 {
+			break
+
 		}
 	}
 	return letter
