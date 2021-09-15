b0337d45ef645680566ca0b46962375549afd1cf
xhmyjae <tomatis.mt@gmail.com>
2021-09-15 11:16:48 +0200

    sqrt

--- sqrt.go
+++ sqrt.go
@@ -1,10 +1,14 @@
 package piscine
 
 func Sqrt(nb int) int {
-	res := nb / (nb * nb)
-	if !(res == 0) {
-		return 0
+	if nb != 0 {
+		res := nb / (nb * nb)
+		if !(res == 0) {
+			return 0
+		} else {
+			return res
+		}
 	} else {
-		return res
+		return 0
 	}
 }
