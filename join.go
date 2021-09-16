3ef41352d1f41905a59c7f164e7272e3825acb01
xhmyjae <tomatis.mt@gmail.com>
2021-09-16 21:08:11 +0200

    join

--- join.go
+++ join.go
@@ -3,7 +3,7 @@ package piscine
 func Join(strs []string, sep string) string {
 	var res string
 	for i := 0; i < len(strs); i++ {
-		if i == len(strs) {
+		if i == len(strs)-1 {
 			res += strs[i]
 		} else {
 			res += strs[i] + sep
