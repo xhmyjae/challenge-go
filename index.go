69dd6b0d1a1d5a07fd6ebca7006a7d15fd5cc03f
xhmyjae <tomatis.mt@gmail.com>
2021-09-16 11:13:06 +0200

    index

--- index.go
+++ index.go
@@ -5,7 +5,7 @@ func Index(s string, toFind string) int {
 	if len(s) >= len(toFind) {
 		for index = range s {
 			if toFind == s[index:index+len(toFind)] && index < len(s)-len(toFind)+1 {
-				return index - 1
+				return index
 			} else {
 				index++
 			}
