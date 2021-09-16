7d0f49ec9f147d046fa954cbdf4422019ed42067
xhmyjae <tomatis.mt@gmail.com>
2021-09-16 20:55:44 +0200

    basicjoin

--- basicjoin.go
+++ basicjoin.go
@@ -2,7 +2,7 @@ package piscine
 
 func BasicJoin(elems []string) string {
 	var res string
-	for i := 0; i < len(elems)+1; i++ {
+	for i := 0; i < len(elems); i++ {
 		res += elems[i]
 	}
 	return res
