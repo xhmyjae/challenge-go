20f608cddb77be65726c40e49306543cbe81bc69
xhmyjae <tomatis.mt@gmail.com>
2021-09-16 11:12:37 +0200

    index

--- index.go
+++ index.go
@@ -3,7 +3,7 @@ package piscine
 func Index(s string, toFind string) int {
 	index := 0
 	if len(s) >= len(toFind) {
-		for index, _ = range s {
+		for index = range s {
 			if toFind == s[index:index+len(toFind)] && index < len(s)-len(toFind)+1 {
 				return index - 1
 			} else {
