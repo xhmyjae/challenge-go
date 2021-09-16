62ccc4c340ea57011dfd4595509edf27709c6482
xhmyjae <tomatis.mt@gmail.com>
2021-09-16 14:47:20 +0200

    toupper

--- toupper.go
+++ toupper.go
@@ -1,10 +1,13 @@
 package piscine
 
 func ToUpper(s string) string {
+	var res string
 	for _, cara := range s {
+		res += string(cara)
 		if rune('a') <= cara && cara <= rune('z') {
 			cara -= 32
+			res += string(cara)
 		}
 	}
-	return s
+	return res
 }
