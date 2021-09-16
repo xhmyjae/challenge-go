5cc3ee73848c4fbfebf8d9bef9a8ecf61712023c
xhmyjae <tomatis.mt@gmail.com>
2021-09-16 12:12:27 +0200

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -1,11 +1,14 @@
 package piscine
 
 func IsAlpha(s string) bool {
-	for _, cara := range s {
-		if rune('a') <= cara && cara <= rune('z') || rune('0') <= cara && cara <= rune('9') || len(s) == 0 {
-			return true
-		} else {
-			return false
+	if len(s) == 0 {
+		return true
+	} else {
+		for _, cara := range s {
+			if rune('a') <= cara && cara <= rune('z') || rune('0') <= cara && cara <= rune('9') {
+				return true
+			}
 		}
+		return false
 	}
 }
