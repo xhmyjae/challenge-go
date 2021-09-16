f36625f65bebd89a065b198ef5d1032e3f3940dd
xhmyjae <tomatis.mt@gmail.com>
2021-09-16 12:09:56 +0200

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -4,7 +4,8 @@ func IsAlpha(s string) bool {
 	for _, cara := range s {
 		if rune('a') <= cara && cara <= rune('z') || rune('0') <= cara && cara <= rune('9') || len(s) == 0 {
 			return true
+		} else {
+			return false
 		}
 	}
-	return false
 }
