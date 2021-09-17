8dedcfc79dce3452eb76d9054386d09e78246960
xhmyjae <tomatis.mt@gmail.com>
2021-09-17 12:11:36 +0200

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,22 +1,28 @@
-package piscine
+package main
 
 func Capitalize(s string) string {
 	var res string
-	for _, cara := range s {
-		if !('a' <= cara-1 && cara-1 <= 'z' || '0' <= cara && cara <= '9' || 'A' <= cara-1 && cara-1 <= 'Z') {
-			if 'a' <= cara && cara <= 'z' {
-				cara -= 32
-				res += string(cara)
-			} else if 'A' <= cara && cara <= 'Z' {
-				res += string(cara)
-			} else if '0' <= cara && cara <= '9' {
-				res += string(cara)
+	for index, cara := range s {
+		if index != 0 {
+			var PInd = s[index-1]
+			if !('a' <= PInd && PInd <= 'z' || '0' <= PInd && PInd <= '9' || 'A' <= PInd && PInd <= 'Z') {
+				if 'a' <= cara && cara <= 'z' {
+					cara -= 32
+					res += string(cara)
+				} else {
+					res += string(cara)
+				}
 			} else {
-				res += string(cara)
+				if 'A' <= cara && cara <= 'Z' {
+					cara += 32
+					res += string(cara)
+				} else {
+					res += string(cara)
+				}
 			}
 		} else {
-			if 'A' <= cara && cara <= 'Z' {
-				cara += 32
+			if 'a' <= cara && cara <= 'z' {
+				cara -= 32
 				res += string(cara)
 			} else {
 				res += string(cara)
@@ -24,4 +30,8 @@ func Capitalize(s string) string {
 		}
 	}
 	return res
+}
+
+func main() {
+	print(Capitalize("m]GM|U8PIPBAZ"))
 }
