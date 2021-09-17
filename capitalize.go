a6d8a22325a439e21f67ab71c600c4b7db390fd7
xhmyjae <tomatis.mt@gmail.com>
2021-09-17 11:58:45 +0200

    capitalize

---
+++ capitalize.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Capitalize(s string) string {
+	var res string
+	for _, cara := range s {
+		if !('a' <= cara-1 && cara-1 <= 'z' || '0' <= cara && cara <= '9' || 'A' <= cara-1 && cara-1 <= 'Z') {
+			if 'a' <= cara && cara <= 'z' {
+				cara -= 32
+				res += string(cara)
+			} else if 'A' <= cara && cara <= 'Z' {
+				res += string(cara)
+			} else if '0' <= cara && cara <= '9' {
+				res += string(cara)
+			} else {
+				res += string(cara)
+			}
+		} else {
+			res += string(cara)
+		}
+	}
+	return res
+}
