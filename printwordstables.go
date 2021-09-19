a14dfae4e909ad35dc7d3bdf9468fd0dc8db01bf
xhmyjae <tomatis.mt@gmail.com>
2021-09-19 02:50:31 +0200

    printwordstables

---
+++ printwordstables.go
@@ -0,0 +1,12 @@
+package piscine
+
+func PrintWordsTables(a []string) {
+	var res string
+	for i := 0; i < len(a); i++ {
+		if i == len(a)-1 {
+			res += a[i]
+		} else {
+			res += a[i] + string('\n')
+		}
+	}
+}
