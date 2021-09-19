43ac6e6dee07b7a85b7230ef77821270ac4430b3
xhmyjae <tomatis.mt@gmail.com>
2021-09-19 12:51:53 +0200

    printwordstables

--- printwordstables.go
+++ printwordstables.go
@@ -1,15 +1,7 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func PrintWordsTables(a []string) {
-	var res string
 	for i := 0; i < len(a); i++ {
-		if i == len(a)-1 {
-			res += a[i]
-		} else {
-			res += a[i] + string('\n')
-		}
+		a = append(a, string('\n'))
 	}
-	z01.PrintRune(a)
 }
