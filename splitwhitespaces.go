19a6732376d5211e6eeb0a360af5998cdaa46422
xhmyjae <tomatis.mt@gmail.com>
2021-09-19 02:07:51 +0200

    splitwhitespaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,7 +4,7 @@ func SplitWhiteSpaces(s string) []string {
 	var arr []string
 	var nstr string
 	for index, cara := range s {
-		if index != 0 && index != len(s) {
+		if index != 0 && index != len(s)+1 {
 			PInd := s[index-1]
 			RPInd := rune(PInd)
 			if Iswhitespaces(RPInd) {
@@ -19,7 +19,7 @@ func SplitWhiteSpaces(s string) []string {
 					nstr = ""
 				}
 			}
-		} else if index == len(s) {
+		} else if index == len(s)+1 {
 			arr = append(arr, string(nstr))
 			nstr = ""
 		} else {
