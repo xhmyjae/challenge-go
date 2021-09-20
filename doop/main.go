4a51c13dfe6ab951d3156962111bfa684b9f9eca
xhmyjae <tomatis.mt@gmail.com>
2021-09-20 10:27:41 +0200

    doop

--- doop/main.go
+++ doop/main.go
@@ -1,27 +1,29 @@
 package main
 
+import "fmt"
+
 func Doop(valueA int, valueB int, op string) {
 	switch op {
 	case "-":
-		print(valueA - valueB)
+		fmt.Println(valueA - valueB)
 	case "+":
-		print(valueA + valueB)
+		fmt.Println(valueA + valueB)
 	case "*":
-		print(valueA * valueB)
+		fmt.Println(valueA * valueB)
 	case "/":
 		if valueB == 0 {
-			print("No division by 0")
+			fmt.Println("No division by 0")
 		} else {
-			print(valueA / valueB)
+			fmt.Println(valueA / valueB)
 		}
 	case "%":
 		if valueB == 0 {
-			print("No modulo by 0")
+			fmt.Println("No modulo by 0")
 		} else {
-			print(valueA % valueB)
+			fmt.Println(valueA % valueB)
 		}
 	default:
-		print("")
+		fmt.Println("")
 	}
 }
 
