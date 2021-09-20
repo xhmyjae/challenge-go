da34184c4028e01f65ae74800f17463c9d00e56e
xhmyjae <tomatis.mt@gmail.com>
2021-09-20 21:51:21 +0200

    printnbr

---
+++ listpushback.go
@@ -0,0 +1,15 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+
+}
