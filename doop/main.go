53f3a81c8cb68ae171e90e8629c3e4c978a10aed
xhmyjae <tomatis.mt@gmail.com>
2021-09-20 11:25:56 +0200

    doop

--- doop/main.go
+++ doop/main.go
@@ -7,26 +7,26 @@ func Doop(valueA int, valueB int, op string) {
 	switch op {
 	case "-":
 		a = valueA - valueB
-		os.Stderr.WriteString(string(rune(a)))
+		os.Stderr.WriteString(string(rune(a + '0')))
 	case "+":
 		a = valueA + valueB
-		os.Stderr.WriteString(string(rune(a)))
+		os.Stderr.WriteString(string(rune(a + '0')))
 	case "*":
 		a = valueA * valueB
-		os.Stderr.WriteString(string(rune(a)))
+		os.Stderr.WriteString(string(rune(a + '0')))
 	case "/":
 		if valueB == 0 {
 			os.Stderr.WriteString("No division by 0")
 		} else {
 			a = valueA / valueB
-			os.Stderr.WriteString(string(rune(a)))
+			os.Stderr.WriteString(string(rune(a + '0')))
 		}
 	case "%":
 		if valueB == 0 {
 			os.Stderr.WriteString("No modulo by 0")
 		} else {
 			a = valueA % valueB
-			os.Stderr.WriteString(string(rune(a)))
+			os.Stderr.WriteString(string(rune(a + '0')))
 		}
 	default:
 		os.Stderr.WriteString("")
