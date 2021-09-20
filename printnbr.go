640813638e1285eed092e9996704629faac4a7e6
xhmyjae <tomatis.mt@gmail.com>
2021-09-20 19:10:15 +0200

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -4,36 +4,17 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintNbr(nbr int) {
-	str := ""
-	isNegative := nbr < 0
-
-	for i, remainder := nbr, 0; i != 0; i /= 10 {
-		remainder = i % 10
-		if isNegative {
-			remainder = -remainder
-		}
-		char := rune(remainder) + rune('0')
-		str += string(char)
+func PrintNbr(n int) {
+	var arr []int
+	num := n
+	for !(num < 10) {
+		nb := num % 10
+		arr = append(arr, nb)
+		num /= 10
 	}
-
-	if isNegative {
-		str += "-"
-	} else if nbr == 0 {
-		str = "0"
+	var v rune
+	for _, each := range arr {
+		v = rune('0' + each)
+		z01.PrintRune(v)
 	}
-
-	str = Reverse(str)
-
-	for _, char := range str {
-		z01.PrintRune(char)
-	}
-}
-
-func Reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
 }
