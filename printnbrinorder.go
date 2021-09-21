0bdc61d44d384df830890634274924a9dbcee4a5
xhmyjae <tomatis.mt@gmail.com>
2021-09-21 15:59:11 +0200

    printnbrinorder

---
+++ printnbrinorder.go
@@ -0,0 +1,29 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) string {
+	var arr []int
+	num := n
+	for !(num < 10) {
+		nb := num % 10
+		arr = append(arr, nb)
+		num /= 10
+	}
+	var SortedArr []int
+	SortedArr = BubbleSort(arr)
+	for _, each := range SortedArr {
+		z01.PrintRune(rune('0' + each))
+	}
+}
+
+func BubbleSort(array []int) []int {
+	for i := 0; i < len(array)-1; i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+	return array
+}
