e6b70b38cb6d17f81fff6ca6a1653ea5ab25b8bf
xhmyjae <tomatis.mt@gmail.com>
2021-09-21 14:55:34 +0200

    listpushback

--- listpushback.go
+++ listpushback.go
@@ -13,6 +13,10 @@ type List struct {
 func ListPushBack(l *List, data interface{}) {
 	a := &NodeL{Data: data} // créer le node qu'on veut à la fin et le & car list a un type pointeur
 	b := l.Head
+	if b == nil {
+		l.Head = a
+		return
+	}
 	for b.Next != nil {
 		b = b.Next
 	}
