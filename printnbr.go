f770b6893cac624a4527f67bf1816596881f9d7f
xhmyjae <tomatis.mt@gmail.com>
2021-09-21 10:58:57 +0200

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -9,21 +9,13 @@ func PrintNbr(n int) {
 	num := n
 	if n < 0 {
 		num = -n
+		z01.PrintRune('-')
 	}
 	var nb int
 	for num != 0 {
 		nb = num % 10
 		arr = append(arr, nb)
+		z01.PrintRune(rune('0' + num))
 		num /= 10
-	}
-	if n < 0 {
-		z01.PrintRune('-')
-		for i := len(arr) - 1; i >= 0; i-- {
-			z01.PrintRune(rune(arr[i]))
-		}
-	} else {
-		for i := len(arr) - 1; i >= 0; i-- {
-			z01.PrintRune(rune(arr[i]))
-		}
 	}
 }
