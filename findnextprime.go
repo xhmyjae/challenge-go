104b68c700481e7304ffee925d2373bfe2c51cc0
xhmyjae <tomatis.mt@gmail.com>
2021-09-21 11:53:35 +0200

    finddnextprime

--- findnextprime.go
+++ findnextprime.go
@@ -1,18 +1,9 @@
 package piscine
 
-func IsPrime(nb int) bool {
-	for i := 2; i <= int((nb)/2); i++ {
-		if nb%i == 0 {
-			return false
-		}
-	}
-	return nb > 1
-}
-
 func FindNextPrime(nb int) int {
 	var res int
 	for j := nb; j >= nb; j++ {
-		for IsPrime(j) == true {
+		for IsPrime(j) {
 			res = j
 		}
 	}
