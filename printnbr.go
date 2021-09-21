033f9bb584a4b63c479750de579b9f7a3830d7e1
xhmyjae <tomatis.mt@gmail.com>
2021-09-21 11:00:37 +0200

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -5,7 +5,7 @@ import (
 )
 
 func PrintNbr(n int) {
-	var arr []int
+	//var arr []int
 	num := n
 	if n < 0 {
 		num = -n
@@ -14,8 +14,18 @@ func PrintNbr(n int) {
 	var nb int
 	for num != 0 {
 		nb = num % 10
-		arr = append(arr, nb)
-		z01.PrintRune(rune('0' + num))
+		//arr = append(arr, nb)
+		z01.PrintRune(rune('0' + nb))
 		num /= 10
 	}
+	/*if n < 0 {
+		z01.PrintRune('-')
+		for i := len(arr) - 1; i >= 0; i-- {
+			z01.PrintRune(rune(arr[i]))
+		}
+	} else {
+		for i := len(arr) - 1; i >= 0; i-- {
+			z01.PrintRune(rune(arr[i]))
+		}
+	}*/
 }
