58743f68488f2891186da5ab22504a50c137917d
xhmyjae <tomatis.mt@gmail.com>
2021-09-22 11:05:02 +0200

    listsize

--- listsize.go
+++ listsize.go
@@ -2,10 +2,10 @@ package piscine
 
 func ListSize(l *List) int {
 	b := l.Head
-	compteur := 1
+	compteur := 0
 	for b.Next != nil {
 		b = b.Next
 		compteur++
 	}
-	return compteur
+	return compteur + 1
 }
