ef74d0e864af7aaf83566b76b03617c6e63d215b
xhmyjae <tomatis.mt@gmail.com>
2021-09-25 17:43:20 +0200

    printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,10 +9,9 @@ import (
 var Args []string
 
 func main() {
-	if len(os.Args[0]) == 0 {
-		z01.PrintRune(rune(' '))
-	}
-	for i := 0; i < len(os.Args[0]); i++ {
-		z01.PrintRune(rune((os.Args[0])[i]))
+	programName := os.Args[0]
+
+	for _, cara := range programName {
+		z01.PrintRune(cara)
 	}
 }
