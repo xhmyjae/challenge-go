7d050bec0d033cb7b05b6c6099c8049ec1dca24d
xhmyjae <tomatis.mt@gmail.com>
2021-09-25 15:45:00 +0200

    printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"os"
-
-	"github.com/01-edu/z01"
 )
 
 var Args []string
 
+func PrintProgName() string {
+	return os.Args[0]
+}
+
 func main() {
-	for i := 0; i < len(os.Args[0]); i++ {
-		z01.PrintRune(rune((os.Args[0])[i]))
-	}
+	PrintProgName()
 }
