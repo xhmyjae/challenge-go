c8620a163c0e986bdac967ca8d498bbd6ab4f329
xhmyjae <tomatis.mt@gmail.com>
2021-09-25 18:51:18 +0200

    printparams

---
+++ printparams/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+var Args []string
+
+func main() {
+	programArgs := os.Args[1:]
+
+	for index := range programArgs {
+		for _, cara := range programArgs[index] {
+			z01.PrintRune(rune(cara))
+		}
+	}
+}
