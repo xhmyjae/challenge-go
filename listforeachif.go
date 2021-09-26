289e9350026c256a5c0b83c3b9409880562d1abb
xhmyjae <tomatis.mt@gmail.com>
2021-09-26 23:07:41 +0200

    listforeachif

---
+++ listforeachif.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+
+	iter := l.Head
+	for iter != nil {
+		if cond(iter) {
+			f(iter)
+		}
+		iter = iter.Next
+	}
+
+}
