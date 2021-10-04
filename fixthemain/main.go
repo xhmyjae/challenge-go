4eef995bdaa0ec678b0fbb651e0615f2c9ee9614
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 16:06:03 +0200

    fixthemain

--- fixthemain/main.go
+++ fixthemain/main.go
@@ -15,6 +15,7 @@ func PrintStr(s string) {
 	for _, r := range s {
 		z01.PrintRune(r)
 	}
+	z01.PrintRune('\n')
 }
 
 func OpenDoor(ptrDoor *Door) bool {
@@ -48,6 +49,7 @@ func IsDoorClose(ptrDoor *Door) bool {
 func main() {
 	door := &Door{}
 
+	OpenDoor(door)
 	if IsDoorClose(door) {
 		OpenDoor(door)
 	}
