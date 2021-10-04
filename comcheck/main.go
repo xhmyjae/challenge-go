55d85fe9f079301a3bdfdd904e4e1d6c79137671
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 14:27:38 +0200

    comcheck

--- comcheck/main.go
+++ comcheck/main.go
@@ -1,6 +1,6 @@
 package main
 
-func ComCheck(str []string) string{
+func ComCheck(str []string) string {
 	for _, each := range str {
 		if each == "01" || each == "galaxy" || each == "galaxy 01" {
 			return "Alert!!!"
