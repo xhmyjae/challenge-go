05fba24d46a88836ca8d456347f37627806351ea
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 19:06:09 +0200

    unmatch

--- unmatch.go
+++ unmatch.go
@@ -1,15 +1,33 @@
 package piscine
 
-func Unmatch(a []int) int {
-	v := BubbleSort(a)
-	for i := 0; i < len(v); i += 2 {
-		for j := i + 1; j < len(v); j += 2 {
-			if a[i] == a[j] {
+func Unmatch(arr []int) int {
+
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				arr[i] = -1
+				arr[j] = -1
 				break
-			} else {
-				return a[i]
 			}
+		}
+	}
+	for k := 0; k < len(arr); k++ {
+		if arr[k] != -1 {
+
+			return arr[k]
 		}
 	}
 	return -1
 }
+
+/*func Unmatch(a []int) int {
+	v := BubbleSort(a)
+	for i := 0; i < len(v); i += 2 {
+		if a[i] == a[i+1] {
+			break
+		} else {
+			return a[i]
+		}
+	}
+	return -1
+}*/
