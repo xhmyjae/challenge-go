a5dacb9605ea87eb2cf086b76396029992876963
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 11:21:13 +0200

    rot14

--- rot14.go
+++ rot14.go
@@ -1,12 +1,20 @@
 package piscine
 
-func rot14(sentence string) string {
+func rot14(s string) string {
 	var res string
-	var r rune
-	for _, cara := range sentence {
-		if rune('a') <= cara && cara <= rune('z') || rune('A') <= cara && cara <= rune('Z') {
-			r = cara
-			res += string(r-14)
+	var v rune
+	for _, cara := range s {
+		if rune('a') <= cara && cara <= rune('z') {
+			if !(rune('a') <= cara-14 && cara-14 <= rune('z')) {
+				v = ((cara-14)-rune('a'))-rune('z')
+			}
+			res += string(v)
+		}
+		if rune('A') <= cara && cara <= rune('Z') {
+			if !(rune('A') <= cara-14 && cara-14 <= rune('Z')) {
+				v = ((cara-14)-rune('A'))-rune('Z')
+			}
+			res += string(v)
 		}
 	}
 	return res
