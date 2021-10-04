73157fe605b730e94ecf7671a02d271ca8dd9a85
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 15:17:24 +0200

    enigma

--- enigma.go
+++ enigma.go
@@ -1,10 +1,11 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	x := *******c
-	***a = x
-	x = ****d
-	y := *b
-	****d = y
-	y = ***a
+	x := ***a
+	***a = *******c
+	y := ****d
+	*******c = y
+	z := *b
+	****d = z
+	*b = x
 }
