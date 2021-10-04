fe58bc8751b987a720fb56bc573fc9d061832ea8
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 12:07:41 +0200

    rot14

--- rot14.go
+++ rot14.go
@@ -5,13 +5,13 @@ func Rot14(s string) string {
 	var v rune
 	for _, cara := range s {
 		if rune('a') <= cara && cara <= rune('z') {
-			if !(rune('a') <= cara-14 && cara-14 <= rune('z')) {
-				v = rune(122 - (97 - (cara - 14)))
+			if !(rune('a') <= cara+14 && cara+14 <= rune('z')) {
+				v = rune(96 + ((cara + 14) - 122))
 			}
 			res += string(v)
 		} else if rune('A') <= cara && cara <= rune('Z') {
-			if !(rune('A') <= cara-14 && cara-14 <= rune('Z')) {
-				v = rune(90 - (65 - (cara - 14)))
+			if !(rune('A') <= cara+14 && cara+14 <= rune('Z')) {
+				v = rune(64 + (65 - (cara + 14) - 90))
 			}
 			res += string(v)
 		} else {
