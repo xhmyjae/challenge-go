fde7a4a4504e8ae4a758c2f5b4c2852f13f3ebd7
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 14:04:53 +0200

    collatzcountdown

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -4,11 +4,10 @@ func CollatzCountdown(start int) int {
 	num := start
 	var count int
 	for i := 0; num == 1; i++ {
+		count++
 		if num%2 != 0 {
-			count++
 			num = num / 2
 		} else {
-			count++
 			num = num*3 + 1
 		}
 	}
