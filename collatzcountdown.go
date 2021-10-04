460e941ef45e1a0ef59b1b7af12917b688a3296c
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 14:14:24 +0200

    collatzcountdown

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -2,13 +2,17 @@ package piscine
 
 func CollatzCountdown(start int) int {
 	var count int
-	for start != 1 {
-		count++
-		if start%2 != 1 {
-			start = start / 2
-		} else {
-			start = start*3 + 1
+	if start < 0 {
+		for start != 1 {
+			count++
+			if start%2 == 0 {
+				start /= 2
+			} else {
+				start = start*3 + 1
+			}
 		}
+	} else {
+		return -1
 	}
 	return count
 }
