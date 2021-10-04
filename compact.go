814faf0efc3fb96dfc38e38135e738699c1921d8
xhmyjae <tomatis.mt@gmail.com>
2021-10-04 17:01:40 +0200

    compact

--- compact.go
+++ compact.go
@@ -1,13 +1,14 @@
 package piscine
 
 func Compact(ptr *[]string) int {
-	var s string
 	count := 0
+	var newPtr []string
 	for _, each := range *ptr {
-		if !(each == s) {
+		if !(each == "") {
 			count++
-			*ptr = append(*ptr, each)
+			newPtr = append(newPtr, each)
 		}
 	}
+	*ptr = newPtr
 	return count
 }
