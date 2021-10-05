b7e0284b423d0c8072315caee4fea24165fb7623
xhmyjae <tomatis.mt@gmail.com>
2021-10-05 09:36:17 +0200

    btreeapplyinorder

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -7,10 +7,3 @@ func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 		BTreeApplyInorder(root.Right, f)
 	}
 }
-
-
-If the current node is empty then return.
-Execute the following three operations in a certain order:[5]
-N: Visit the current node.
-L: Recursively traverse the current node's left subtree.
-R: Recursively traverse the current node's right subtree.
