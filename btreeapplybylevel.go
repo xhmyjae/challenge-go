81308623dc3f204944b359b82bb6f44e1f5697af
xhmyjae <tomatis.mt@gmail.com>
2021-10-05 18:44:42 +0200

    btreeapplybylevel

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -4,19 +4,20 @@ func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root != nil {
 		left := root.Left
 		right := root.Right
-		// var queue []string
-		// queue = append(queue, root.Data)
-		// for len(queue) > 0 {
-		// curr := queue[0]
-		// queue = queue[0:]
-		for i := BTreeLevelCount(root); i > 0; i-- {
+		var queue []*TreeNode
+		queue = append(queue, root)
+		for len(queue) > 0 {
+			curr := queue[0]
+			queue = queue[0:]
+			// for i := BTreeLevelCount(root); i > 0; i-- {
 			if left != nil {
-				// queue = append(queue, left)
-				BTreeApplyByLevel(left, f)
+				queue = append(queue, left)
+				f(curr)
+				// BTreeApplyByLevel(left, f)
 			}
 			if right != nil {
-				// queue = append(queue, right)
-				BTreeApplyByLevel(right, f)
+				queue = append(queue, right)
+				// BTreeApplyByLevel(right, f)
 			}
 			// }
 		}
